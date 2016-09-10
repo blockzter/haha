9ef2a903b93c8583d8e680057e0abc51fc06f045
blockm <mblock@gmail.com>
2016-09-10 11:38:04 -0400

    use props for mqtt params

---
+++ src/main/java/org/blockzter/zmon/exceptions/PropertyNotFoundException.java
@@ -0,0 +1,27 @@
+package org.blockzter.zmon.exceptions;
+
+/**
+ * Created by blockm on 9/10/16.
+ *
+ */
+public class PropertyNotFoundException extends Exception {
+	public PropertyNotFoundException() {
+		super();
+	}
+
+	public PropertyNotFoundException(String message) {
+		super(message);
+	}
+
+	public PropertyNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public PropertyNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+	protected PropertyNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+}
--- src/main/java/org/blockzter/zmon/mqtt/MonitorSubs.java
+++ src/main/java/org/blockzter/zmon/mqtt/MonitorSubs.java
@@ -1,21 +1,31 @@
 package org.blockzter.zmon.mqtt;
 
+import org.blockzter.zmon.exceptions.PropertyNotFoundException;
 import org.blockzter.zmon.mqtt.client.ZWaveClient;
+import org.blockzter.zmon.utils.AppUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Properties;
+
 /**
  * Created by mblock2 on 8/19/16.
+ *
  */
 public class MonitorSubs {
 	private static Logger LOGGER = LoggerFactory.getLogger(MonitorSubs.class);
-	private String brokerUrl = "tcp://192.168.1.42:1883";
+	private static final String CLIENT_PROP_FILENAME_KEY = "CONFIG";
+	private static final String CLIENT_PROP_FILENAME = "client.properties";
+	private static final String MQTT_CLIENT_ID_KEY = "mqtt.client.id";
+	private static final String MQTT_BROKER_URL_KEY = "mqtt.broker.url";
+
+	private String brokerUrl;
 	private String userId;
 	private String password;
-	private String clientId = "MON";
+	private String clientId;
 
 
-	public MonitorSubs(String userId, String password) {
+	private MonitorSubs(String userId, String password) {
 		this.userId = userId;
 		this.password = password;
 	}
@@ -24,9 +34,29 @@ public class MonitorSubs {
 		MonitorSubs mon = new MonitorSubs(args[0], args[1]);
 
 		mon.startMonitoring();
+		LOGGER.info("Laters...");
+	}
+
+	private void init() {
+		Properties properties = null;
+		try {
+			properties = AppUtils.loadProperties(CLIENT_PROP_FILENAME_KEY);
+			LOGGER.info("LOAD props={}", properties);
+		} catch (Exception e) {
+			LOGGER.error("Failed to load properties file {}", CLIENT_PROP_FILENAME, e);
+			System.exit(-1);
+		}
+		try {
+			clientId = AppUtils.getProperty(properties, MQTT_CLIENT_ID_KEY);
+			brokerUrl = AppUtils.getProperty(properties, MQTT_BROKER_URL_KEY);
+		} catch(PropertyNotFoundException e) {
+			LOGGER.error("init failed ", e);
+			System.exit(-2);
+		}
 	}
 
 	private void startMonitoring() {
+		init();
 		ZWaveClient client = new ZWaveClient(brokerUrl, userId, password, clientId);
 
 		try {
